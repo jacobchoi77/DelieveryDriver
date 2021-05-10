17c3161d98e89e7573496d148c647da9a4124852
Rick Davidson <rickinaction@gmail.com>
2021-05-10 11:34:17 +1000

    5 Introducing Variables

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+    
     void Start()
     {
         
@@ -11,7 +14,7 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
